d7c7d01c55ce5dc5b44dbc2fe9511ab9f705b6d3
dantokoro <duongdang0508@gmail.com>
2021-10-20 22:28:53 +0700

    hobby done + grid

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -18,42 +18,53 @@ const Hobby = () => {
       <div className="hobby-title d-flex justify-content-center">
         <h2>SỞ THÍCH</h2>
       </div>
-      <div className="hobby-list d-flex justify-content-around">
-        <motion.button variants={hoverVariants} whileHover="hover">
-          Võ thuật
-        </motion.button>
-        <motion.button variants={hoverVariants} whileHover="hover">
-          Trau dồi kiến thức
-        </motion.button>
-        <motion.button variants={hoverVariants} whileHover="hover">
-          Trải nghiệm
-        </motion.button>
+      <div className="hobby-list container">
+        <div class="row">
+          <div class="col d-flex flex-column align-items-center">
+            <motion.button variants={hoverVariants} whileHover="hover">
+              Võ thuật
+            </motion.button>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <motion.button variants={hoverVariants} whileHover="hover">
+              Trau dồi kiến thức
+            </motion.button>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <motion.button variants={hoverVariants} whileHover="hover">
+              Trải nghiệm
+            </motion.button>
+          </div>
+        </div>
       </div>
       <div className="hobby-content container">
         <p>
-          Mình đã tập võ tổng cộng được khoảng 3 4 năm gì đó. Dưới đây là cảm
-          nhận của mình với những môn võ đã tập qua.
+          Mình đã tập võ tổng cộng được khoảng 3 4 năm. Đây là các môn võ mình
+          đã tập.
         </p>
-        <p>
-          <b>Vịnh Xuân</b> là môn võ với nền tảng lấy nhu chế cương. Lối đánh
-          của môn võ này rất dẻo dai, uyển chuyển, thiên về đỡ gạt và phản đòn.
-          Chúng ta thường bắt gặp những ông cụ tập Vịnh Xuân ở công viên một
-          cách nhẹ nhàng, điềm đạm với mục đích rèn luyện sức khỏe. Hay đôi khi
-          cũng thấy một số võ sư Vịnh Xuân bị các võ sĩ Boxing đấm sấp mặt trên
-          sàn đấu. Tuy nhiên mình tin Vịnh Xuân không hề yếu, chỉ là không hợp
-          thời thế. Vịnh Xuân, hay những môn võ cổ truyền của Việt Nam và Trung
-          Quốc nói chung, sinh ra trong thời chiến tranh vũ khí lạnh, do đó các
-          môn võ này sẽ mạnh ở những đòn đánh hiểm hóc, triệt hạ, hay để giết
-          người. Đây là những điều không phù hợp với thời hiện đại, khi mà võ
-          thuật chủ yếu để rèn luyện sức khỏe và thi đấu thể thao. Cá nhân mình
-          thích Vịnh Xuân ở sự điềm đạm, nhẹ nhàng, triết lý lấy nhu chế cương
-          khác những môn võ hổ báo mà chúng ta thường gặp.
-        </p>
-        <p>
-          Taekwondo là môn võ thiên về sử dụng các đòn đá. Mình từng bị trấn
-          thương nhẹ cơ chân do tập lung tung mà không khởi động. Cũng vì trấn
-          thương này mà mình nghỉ luôn CLB Taekwondo.
-        </p>
+        <div class="row">
+          <div class="col d-flex flex-column align-items-center">
+            <img
+              src="https://res.cloudinary.com/dbzfjnlhl/image/upload/v1634740566/Portfolio/52c92ad593ac34a09275e4bfa122f7e3_1_ou8mi4.png"
+              alt="wingchun"
+            />
+            <h5>Vịnh Xuân Quyền</h5>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <img
+              src="https://res.cloudinary.com/dbzfjnlhl/image/upload/v1634740566/Portfolio/63f6f1641e81fc651a0d7ff7261ebc56_1_cgd048.png"
+              alt="taekwondo"
+            />
+            <h5>Taekwondo</h5>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <img
+              src="https://res.cloudinary.com/dbzfjnlhl/image/upload/v1634740566/Portfolio/copy_96188495_mbr9yk.png"
+              alt="arnis"
+            />
+            <h5>Arnis</h5>
+          </div>
+        </div>
       </div>
     </div>
   );
